aa3aa0a42a474936740855e8154e79d0c8764a63
Dmitry <dmitry27zh@gmail.com>
2023-07-28 18:00:24 +0300

    Start counter component

---
+++ src/components/counter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Counter = () => {
+  const count = 0
+
+  return (
+    <>
+      <span>{count}</span>
+      <button></button>
+    </>
+  )
+}
+
+export default Counter
