08f328ae8cad22d68cb889b03fc9d2431ac1777b
Dmitry <dmitry27zh@gmail.com>
2023-07-30 11:20:56 +0300

    Render Counter component, handle click

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,11 +2,25 @@ import React from 'react'
 
 const Counter = () => {
   const count = 0
+  const formatCount = () => {
+    return count === 0 ? 'empty' : count
+  }
+  const getBageClasses = () => {
+    let classes = 'badge m-2 '
+    classes += count === 0 ? 'text-bg-warning' : 'text-bg-primary'
+
+    return classes
+  }
+  const handleClick = () => {
+    console.log('Click!!!')
+  }
 
   return (
     <>
-      <span>{count}</span>
-      <button></button>
+      <span className={getBageClasses()}>{formatCount()}</span>
+      <button className="btn btn-primary btn-sm m-2" onClick={handleClick}>
+        +
+      </button>
     </>
   )
 }
