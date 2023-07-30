7c0f0d8e6a74df9863e341f8b238ac5d78dfa57a
Dmitry <dmitry27zh@gmail.com>
2023-07-30 12:04:42 +0300

    Practise conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -21,16 +21,24 @@ const Counter = () => {
   const handleTagChange = (id) => {
     setTags((prevState) => prevState.filter((tag) => tag !== id))
   }
+  const renderTags = () => {
+    if (tags.length === 0) {
+      return 'No tags'
+    }
+
+    return tags.map((tag) => (
+      <li key={tag} className="btn btn-primary btn-sm m-2" onClick={() => handleTagChange(tag)}>
+        {tag}
+      </li>
+    ))
+  }
+
+  if (tags.length !== 0) {
+    return <ul>{renderTags()}</ul>
+  }
 
   return (
     <>
-      <ul>
-        {tags.map((tag) => (
-          <li key={tag} className="btn btn-primary btn-sm m-2" onClick={() => handleTagChange(tag)}>
-            {tag}
-          </li>
-        ))}
-      </ul>
       <span className={getBageClasses()}>{formatCount()}</span>
       <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
         +
