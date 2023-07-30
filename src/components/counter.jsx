14ffef6fc21f1d49b3ceecf8d8ba9a2b8d3c3161
Dmitry <dmitry27zh@gmail.com>
2023-07-30 11:57:07 +0300

    Practise useState with array

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 const Counter = () => {
   const [count, setCount] = useState(0)
+  const [tags, setTags] = useState(['tag1', 'tag2', 'tag3'])
   const formatCount = () => {
     return count === 0 ? 'empty' : count
   }
@@ -17,9 +18,19 @@ const Counter = () => {
   const handleDecrement = () => {
     setCount((prevState) => prevState - 1)
   }
+  const handleTagChange = (id) => {
+    setTags((prevState) => prevState.filter((tag) => tag !== id))
+  }
 
   return (
     <>
+      <ul>
+        {tags.map((tag) => (
+          <li key={tag} className="btn btn-primary btn-sm m-2" onClick={() => handleTagChange(tag)}>
+            {tag}
+          </li>
+        ))}
+      </ul>
       <span className={getBageClasses()}>{formatCount()}</span>
       <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
         +
