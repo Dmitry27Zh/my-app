fa9ef06cf3150d18a38cb29adbec420ba5803cd2
Dmitry <dmitry27zh@gmail.com>
2023-07-31 12:19:36 +0300

    Pass on value in Counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,22 +1,21 @@
 import React, { useState } from 'react'
 
-const Counter = (param) => {
-  console.log(param)
-  const [count, setCount] = useState(0)
+const Counter = (props) => {
+  const [value, setValue] = useState(props.value)
   const formatCount = () => {
-    return count === 0 ? 'empty' : count
+    return value === 0 ? 'empty' : value
   }
   const getBageClasses = () => {
     let classes = 'badge m-2 '
-    classes += count === 0 ? 'text-bg-warning' : 'text-bg-primary'
+    classes += value === 0 ? 'text-bg-warning' : 'text-bg-primary'
 
     return classes
   }
   const handleIncrement = () => {
-    setCount((prevState) => prevState + 1)
+    setValue((prevState) => prevState + 1)
   }
   const handleDecrement = () => {
-    setCount((prevState) => prevState - 1)
+    setValue((prevState) => prevState - 1)
   }
 
   return (
--- src/components/counterList.jsx
+++ src/components/counterList.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Counter from './counter'
 
 const CounterList = () => {
-  const counters = [{ id: 0 }, { id: 1 }, { id: 2 }]
+  const counters = [
+    { id: 0, value: 0 },
+    { id: 1, value: 0 },
+    { id: 2, value: 3 },
+  ]
 
   return (
     <div>
-      {counters.map((count, index) => (
-        <Counter key={index}></Counter>
+      {counters.map((count) => (
+        <Counter key={count.id} value={count.value}></Counter>
       ))}
     </div>
   )
